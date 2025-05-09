f0489e264a2bf83a9bce369d355c19e250a41394
szymon tracz <s.tracz@blachotrapez.com.pl>
2025-05-09 10:29:01 +0200

    Remove outdated resource configurations and refactor services
    Deleted unused YAML configurations for accounts, cards, and loans. Updated service names, application names, and configurations for better consistency (e.g., configserver, eurekaserver). Added new GatewayServer module and integrated necessary dependencies and configurations.

--- build-all.ps1
+++ build-all.ps1
@@ -1,4 +1,4 @@
-$services = @("auth-server", "config-server", "eureka-server", "user-service")
+$services = @("auth-server", "configserver", "eurekaserver", "user-service")
 
 foreach ($s in $services) {
     $imageFullName = "szopszop/$s"
