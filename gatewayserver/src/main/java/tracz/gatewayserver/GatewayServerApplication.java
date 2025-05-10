b730de79bb7d66e49a53f23b37345b0f5737fed9
szymon tracz <s.tracz@blachotrapez.com.pl>
2025-05-10 08:50:14 +0200

    Add custom route filters to Gateway Server
    Implemented route definitions with path rewriting and response header addition for `/whatsapp/user-service/**` and `/whatsapp/auth-server/**`. Disabled discovery locator to use explicit route configurations.

--- gatewayserver/src/main/java/tracz/gatewayserver/GatewayServerApplication.java
+++ gatewayserver/src/main/java/tracz/gatewayserver/GatewayServerApplication.java
@@ -2,12 +2,32 @@ package tracz.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/whatsapp/user-service/**")
+						.filters( f -> f.rewritePath("/whatsapp/user-service/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://user-service"))
+				.route(p -> p
+						.path("/whatsapp/auth-server/**")
+						.filters( f -> f.rewritePath("/whatsapp/auth-server/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://auth-server"))
+		.build();
 	}
 
 }
