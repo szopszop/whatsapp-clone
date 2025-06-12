9bf7e4ae67adb00d34816b89875e8eec0f7b830d
szymon tracz <s.tracz@blachotrapez.com.pl>
2025-06-12 11:16:45 +0200

    Refactor auth-server and enhance persistence setup
    Removed deprecated tests, introduced new integration tests, and reorganized configurations for environments and entities. Added Flyway migrations, enabled Redis caching, and improved JPA auditing to support dynamic user logging. Simplified role fetching and enhanced token blacklist functionality with additional tests.

--- auth-server/src/main/java/tracz/authserver/dto/RegisterRequest.java
+++ auth-server/src/main/java/tracz/authserver/dto/RegisterRequest.java
@@ -4,15 +4,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class RegisterRequest {
+public record RegisterRequest (
     @Email
-    private String email;
+    String email,
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,100}$",
             message = "Password must be at least 8 characters long, with 1 uppercase letter, "
                     + "with 1 lower case letter, with 1 special character")
-    private String password;
-}
+    String password) {}
