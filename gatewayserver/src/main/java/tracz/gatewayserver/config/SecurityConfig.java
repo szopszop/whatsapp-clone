ce630b8b24a9b4836de42f32d1521e3e76f5d5e5
szymon tracz <s.tracz@blachotrapez.com.pl>
2025-06-18 14:17:28 +0200

    poprawiony gateway

---
+++ gatewayserver/src/main/java/tracz/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package tracz.gatewayserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withIssuerLocation(issuerUri).build();
+    }
+}
