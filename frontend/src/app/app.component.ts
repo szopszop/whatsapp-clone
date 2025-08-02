0be92f56f75c5e5b31d258d41274b034f1982d8e
szymon tracz <s.tracz@blachotrapez.com.pl>
2025-08-02 18:59:06 +0200

    Add Google Tag Manager and tweak service configurations

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,33 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet],
-    template: '<router-outlet></router-outlet>',
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      if (window.dataLayer) {
+        window.dataLayer.push({
+          event: 'page_view',
+          page_path: event.urlAfterRedirects
+        });
+      }
+    });
+  }
 }
